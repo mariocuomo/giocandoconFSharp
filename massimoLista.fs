05be23a24809abc850d121311363110af06fdf44
mariocuomo <cuomomario@hotmail.com>
2022-01-24 21:28:03 +0100

    massimoLista

--- massimoLista.fs
+++ massimoLista.fs
@@ -1,11 +1,21 @@
 let lista = [1;2;3;4;5;6;7;8;9]
 
+(*
+massimo: int -> int -> int
+massimo x y = massimo valore tra x e y
+*)
 let massimo x y =
     if x>y then x
     else y
 
+(*
+massimoLst: int list -> int
+massimoLst lst = massimo valore in lst
+*)
 let rec massimoLst lst =
         match lst with
         | [] -> -14
         | x::[y] -> massimo x y
         | x::rest -> massimo x (massimoLst rest)
+
+printfn "%d" (massimoLst lista)
